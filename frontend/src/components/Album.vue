a02092929ed3dc4e3ac775d8e64270b1b22adae7
David Grzyb <grzybdavid@gmail.com>
2020-07-03 16:22:48 -0400

    Added frontend Vue application
    Installed TailwindCSS and setup PurgeCSS
    
    Created components and added dynamic content
    
    Moved public images to assets directory
    
    Minor formatting fix
    
    Updated error handling

---
+++ frontend/src/components/Album.vue
@@ -0,0 +1,21 @@
+<template>
+    <main class="grid grid-cols-1 md:grid-cols-3 gap-4 md:px-2 pt-4">
+        <Photo
+            v-for="photo in album" :key="photo.id"
+            :photo=photo />
+    </main>
+</template>
+
+<script>
+import Photo from './Photo.vue'
+
+export default {
+    name: 'Album',
+    components: {
+        Photo
+    },
+    props: {
+        album: Array
+    }
+}
+</script>
---
+++ frontend/src/components/Photo.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="bg-white rounded">
+        <div href="#" class="relative">
+            <img :src="require('../assets/' + photo.img)" :alt="photo.title" class="rounded-t">
+            <a href="#" class="absolute bottom-0 left-0 text-lg font-bold text-white pb-1 pl-2 hover:underline focus:outline-none focus:underline">{{ photo.title }}</a>
+        </div>
+        <div class="flex flex-col px-6 pt-1 pb-4">
+            <p class="text-xxs text-gray-600 text-justify">{{ photo.description }}</p>
+            <div class="flex items-center justify-between pt-1">
+                <span v-if="photo.featured">❤️</span>
+                <span class="text-xxs text-gray-500">{{ photo.date }}</span>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Photo',
+    props: {
+        photo: Object
+    }
+}
+</script>
